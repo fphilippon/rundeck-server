b3b3d5d1760f1db99191ff9503c2d9e772929483
Florian Philippon <florian.philippon@s4m.io>
2017-03-07 16:18:16 +0100

    Fix syntax errors with ruby 2.4

--- resources/project.rb
+++ resources/project.rb
@@ -54,12 +54,10 @@ attribute :executor,
           kind_of: [Symbol, Hash],
           default: :ssh,
           callbacks: {
-            must_contain_provider: lambda do |executor|
-              executor.is_a?(Symbol) || !executor['provider'].nil? || !executor[:provider].nil?
-            end,
-            must_contain_config: lambda do |executor|
-              executor.is_a?(Symbol) || (executor['config'] || executor[:config]).is_a?(Hash)
-            end
+            must_contain_provider: lambda { |executor|
+              executor.is_a?(Symbol) || !executor['provider'].nil? || !executor[:provider].nil? },
+            must_contain_config: lambda { |executor|
+              executor.is_a?(Symbol) || (executor['config'] || executor[:config]).is_a?(Hash) }
           }
 
 # <> @attribute sources List of node sources
@@ -67,12 +65,10 @@ attribute :sources,
           kind_of: Array,
           required: true,
           callbacks: {
-            must_be_an_array_of_hashes: lambda do |sources|
-              sources.all? { |source| source.is_a?(Hash) }
-            end,
-            must_contain_type: lambda do |sources|
-              sources.all? { |source| source['type'] || source[:type] }
-            end
+            must_be_an_array_of_hashes: lambda { |sources|
+              sources.all? { |source| source.is_a?(Hash) }},
+            must_contain_type: lambda { |sources|
+              sources.all? { |source| source['type'] || source[:type] }}
           }
 
 attribute :cookbook,
